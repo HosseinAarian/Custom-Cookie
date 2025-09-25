fe033532a27a63f5ab8f00ecb8671828445c5f2d
HosseinArian <hoseinaryan22@gmail.com>
2025-09-25 12:35:42 +0330

    Create Project

---
+++ CustomCookie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CustomCookie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
